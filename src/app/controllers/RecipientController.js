7a16da70a849d67816fd4132a9706ffae275bace
esoft11 - Daniel Barreto <dandpb@gmail.com>
2020-03-19 17:36:58 -0300

    adicionado desafio 2

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,83 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+const schemaValidation = Yup.object().shape({
+  name: Yup.string().required(),
+  street: Yup.string().required(),
+  number: Yup.number().required(),
+  complement: Yup.string(),
+  state: Yup.string()
+    .required()
+    .max(2),
+  city: Yup.string().required(),
+  zip_code: Yup.string().required(),
+});
+
+class RecipientController {
+  async store(req, res) {
+    if (!(await schemaValidation.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip_code,
+    } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip_code,
+    });
+  }
+
+  async update(req, res) {
+    if (!(await schemaValidation.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+    let recipient = await Recipient.findByPk(id);
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient not found' });
+    }
+
+    await Recipient.update(req.body, { where: { id } });
+
+    recipient = await Recipient.findByPk(id);
+    return res.json(recipient);
+  }
+
+  async destroy(req, res) {
+    const { recipientId } = req.params;
+    const recipient = await Recipient.findByPk(recipientId);
+    if (!recipient) {
+      return res.status(500).json({ error: 'Recipient not found' });
+    }
+
+    await Recipient.destroy({
+      where: {
+        id: recipientId,
+      },
+    });
+    return res.status(200).send({ message: 'Recipient deleted' });
+  }
+
+  async index(req, res) {
+    const recipients = await Recipient.findAll();
+    return res.json(recipients);
+  }
+}
+
+export default new RecipientController();
